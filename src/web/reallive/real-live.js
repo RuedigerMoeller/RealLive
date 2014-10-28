6cc1ccf09c3992031c85f9b9da57091edefd44c6
RuedigerMoeller <moru0011@gmail.com>
2014-10-28 02:37:27 +0100

    mach2

--- src/web/reallive/real-live.js
+++ src/web/reallive/real-live.js
@@ -1,4 +1,5 @@
 // version 2 of js RealLive facade as callback/connection management moved to kontraktor
+// requires kontraktor.js
 
 var RL_UPDATE    = 0;
 var RL_ADD       = 1;
@@ -8,4 +9,20 @@ var RL_SNAPSHOT_DONE = 4;
 var RL_ERROR = 5;
 
 var RealLive = new function() {
+
+    this.getTableMeta = function(tableId,columnName) {
+        var res = Server.meta().tables[tableId];
+        if ( columnName ) {
+            return res.columns[columnName];
+        }
+        return res;
+    };
+
+    this.getFieldName = function(tableId,fieldId) {
+        if ( this.getTableMeta( tableId) ) {
+            return this.getTableMeta(tableId).fieldId2Name[fieldId];
+        }
+        console.log("unknown table "+tableId+" "+fieldId);
+    };
+
 };
